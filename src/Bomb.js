c946f3ceb12f11e7b386757947bab03c6c7faa14
ParishBen <ParishBen13@gmail.com>
2021-01-24 01:19:03 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+   constructor(){
+       super();
+    this.state = {
+        secondsLeft: props.initialCount
+      }
+    }
+    render() {
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds until I go boom!`;
+        return (
+          <div 
+            className="bomb"
+            //style={{backgroundColor: this.state.color}}
+            //onClick={this.handleClick}>
+            >
+               
+            {message}
+          </div>
+        )
+      }
+      
+    }
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,24 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component {
+    constructor(){
+        super();
+        this.state = {
+            currentSlideIndex: 0
+          }
+
+    }
+    render() {
+        return (
+          <div 
+            className="image-slider"
+            //style={{backgroundColor: this.state.color}}
+            //onClick={this.handleClick}>
+            >
+               { `I am on slide ${this.state.currentSlideIndex}` }
+          </div>
+        )
+      }
+      
+    }
